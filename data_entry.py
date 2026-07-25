2736da5a8e64a2322b20426534330a7a8d4c2be6
Raspberry Pi OS <ethansikes2007@icloud.com>
2026-07-25 00:59:49 -0400

    tool python file created, commented with rough ideas

---
+++ data_entry.py
@@ -0,0 +1,13 @@
+print("This is a universal data entry tool made with Python used to create, edit, and delete JSON files.")
+
+# Universal
+# Schemas?
+# UI: Create, Program Edit, Nano Edit, Delete, Open, Read, Search
+# Create folders?
+# Looping system
+# Call function for each major task
+# Able to Open JSON file to fully read
+# Able to Read a defined amount of the file
+# Able to Search if a defined object is in the file
+# Able to Edit through a designed method via this program (i.e., search for specific object, look through the inside, and edit it)
+# Able to Edit through a Nano call on the file for manual edits
